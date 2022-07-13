0bfbc0a16a720f56d073ade8ce704e850b00ad0d
Rifluxyss Cintas <rifluxyss.cintas@gmail.com>
2022-07-13 20:19:53 +0530

    Insert value replace changes commits

--- Cintas-SDK/src/main/java/com/cintas/cintassdk/EnterPriceLoggingTable.java
+++ Cintas-SDK/src/main/java/com/cintas/cintassdk/EnterPriceLoggingTable.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Dao
 public interface EnterPriceLoggingTable {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     void insertEnterPriceValue(EnterPriceLoggingData enterPriceLoggingData);
 
     @Query("SELECT * FROM PRICE_DETAIL")
